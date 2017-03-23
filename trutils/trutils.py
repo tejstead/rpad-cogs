af2635f8f1cd456f24f3a3b6d6c3f4ec6aeb1c39
nachoapps <nachoappsinc@gmail.com>
2017-03-22 23:32:26 -0400

    changing dumpmsg so it wraps with ` if the message starts OR ends with a box

--- trutils/trutils.py
+++ trutils/trutils.py
@@ -257,10 +257,10 @@ class TrUtils:
 
     @commands.command(pass_context=True, no_pm=True)
     async def dumpmsg(self, ctx, msg_id : int):
-        """Given an ID for a message printed by the bot in the current channel, dumps it boxed with formatting escaped"""
+        """Given an ID for a message printed in the current channel, dumps it boxed with formatting escaped"""
         msg = await self.bot.get_message(ctx.message.channel, msg_id)
         content = msg.clean_content.strip()
-        if content.startswith('```') and content.endswith('```'):
+        if content.startswith('```') or content.endswith('```'):
             content = '`\n{}\n`'.format(content)
         else:
             content = box(content)
