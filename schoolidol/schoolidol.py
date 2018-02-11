1be8257a5fb71b00d2d2feb31f03bff858fa6e5f
nachoapps <nachoappsinc@gmail.com>
2018-02-11 18:33:58 -0500

    Formatting and miru hosting of images

--- schoolidol/schoolidol.py
+++ schoolidol/schoolidol.py
@@ -32,7 +32,7 @@ class SchoolIdol:
 
     async def reload_sif(self):
         await self.bot.wait_until_ready()
-        
+
         next_req = FIRST_REQ
         async with aiohttp.ClientSession() as session:
             while next_req:
@@ -43,15 +43,16 @@ class SchoolIdol:
                     next_req = js_resp['next']
                     self.card_data.extend(js_resp['results'])
         print('done retrieving cards: {}'.format(len(self.card_data)))
-        
-        
+
         self.id_to_card = {c['id']: c for c in self.card_data}
         name_to_card = {'{}'.format(c['idol']['name']).lower(): c for c in self.card_data}
         firstname_to_card = {c['idol']['name'].lower().split(' ')[0]: c for c in self.card_data}
-        collection_name_to_card = {'{} {}'.format(c['translated_collection'], c['idol']['name']).lower(): c for c in self.card_data}
-        collection_firstname_to_card = {'{} {}'.format(c['translated_collection'], c['idol']['name'].split(' ')[0]).lower(): c for c in self.card_data}
-        self.names_to_card = {**firstname_to_card, **name_to_card, **collection_name_to_card, **collection_firstname_to_card}
-        
+        collection_name_to_card = {'{} {}'.format(
+            c['translated_collection'], c['idol']['name']).lower(): c for c in self.card_data}
+        collection_firstname_to_card = {'{} {}'.format(
+            c['translated_collection'], c['idol']['name'].split(' ')[0]).lower(): c for c in self.card_data}
+        self.names_to_card = {**firstname_to_card, **name_to_card, **
+                              collection_name_to_card, **collection_firstname_to_card}
 
     @commands.command(pass_context=True)
     async def sifid(self, ctx, *, query: str):
@@ -63,15 +64,15 @@ class SchoolIdol:
         else:
             c = self.names_to_card.get(query, None)
             if c is None:
-                matches = difflib.get_close_matches(query, self.names_to_card.keys(), n=1, cutoff=.6)
+                matches = difflib.get_close_matches(
+                    query, self.names_to_card.keys(), n=1, cutoff=.6)
                 if len(matches):
                     c = self.names_to_card[matches[0]]
-                    
+
         if c:
             await self.do_menu(ctx, c)
         else:
             await self.bot.say(inline('no matches'))
-
 
     async def do_menu(self, ctx, c):
         emoji_to_embed = OrderedDict()
@@ -79,18 +80,18 @@ class SchoolIdol:
         if regular_embed:
             emoji_to_embed[self.regular_emoji] = regular_embed
             starting_menu_emoji = self.regular_emoji
-            
+
         idol_embed = make_card_embed(c, IDOL_IMAGE_FIELD)
         if idol_embed:
             emoji_to_embed[self.idol_emoji] = idol_embed
             starting_menu_emoji = self.idol_emoji
-            
+
         if starting_menu_emoji is None:
             await self.bot.say(inline('no images found'))
             return None
-        
+
         return await self._do_menu(ctx, starting_menu_emoji, emoji_to_embed)
-    
+
     async def _do_menu(self, ctx, starting_menu_emoji, emoji_to_embed):
         remove_emoji = self.menu.emoji['no']
         emoji_to_embed[remove_emoji] = self.menu.reaction_delete_message
@@ -103,23 +104,27 @@ class SchoolIdol:
                 await self.bot.edit_message(result_msg, embed=result_embed)
         except Exception as ex:
             print('Menu failure', ex)
-            
+
 
 def make_card_embed(c, url_field):
+    cid = c['id']
     image_url = c[url_field]
     if not image_url:
         return None
-    
+
+    base_url = 'https://storage.googleapis.com/mirubot/sifimages/processed/{}_{}.png'
+
     embed = discord.Embed()
     embed.title = toHeader(c)
     embed.url = get_info_url(c)
-    embed.set_image(url='http:{}'.format(image_url))
+    embed.set_image(url=base_url.format(cid, url_field))
     embed.set_footer(text='Requester may click the reactions below to switch tabs')
     return embed
-    
+
 
 IMAGE_FIELD = 'transparent_image'
 IDOL_IMAGE_FIELD = 'transparent_idolized_image'
+
 
 def toHeader(c):
     cid = c['id']
@@ -130,11 +135,14 @@ def toHeader(c):
     else:
         return 'No. {} {}'.format(cid, name)
 
+
 def get_info_url(c):
     return 'https://schoolido.lu/cards/{}/'.format(c['id'])
 
+
 def check_folders():
     pass
+
 
 def check_files():
     pass
