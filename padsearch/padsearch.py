8ab26675286ac9607c15816dfd8b88efca954314
dchang0413 <32379521+dchang0413@users.noreply.github.com>
2018-08-02 04:03:10 -0500

    Update padsearch.py

--- padsearch/padsearch.py
+++ padsearch/padsearch.py
@@ -428,8 +428,8 @@ class SearchConfig(object):
             self.filters.append(lambda m,
                                 tf=text_from,
                                 tt=text_to:
-                                tf in m.search.convert_from and
-                                tt in m.search.convert_to)
+                                (tf in m.search.convert_from1 and tt in m.search.convert_to1) or
+                                (tf in m.search.convert_from2 and tt in m.search.convert_to2))
 
         if self.absorbnull:
             text = 'damage absorb shield'
