b9e438e016ed15b863e6f05f47d5c1fae137a255
Ilya Brin <dontgitit@users.noreply.github.com>
2019-12-10 16:38:08 -0800

    only include jp cards in jp announcements and fix padsearch (#42)
    * only include jp cards in jp announcements
    
    * fix padsearch
    
    * dont need await

--- padsearch/padsearch.py
+++ padsearch/padsearch.py
@@ -655,7 +655,7 @@ class PadSearch:
                 # If it still failed, raise the original exception
                 raise ex
         dg_cog = self.bot.get_cog('Dadguide')
-        monsters = dg_cog.database.all_monsters()
+        monsters = dg_cog.database.get_all_monsters()
         matched_monsters = list(filter(config.check_filters, monsters))
 
         # Removing entry with names that have gems in it
